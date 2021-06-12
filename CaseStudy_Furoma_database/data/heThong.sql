ecc0e60aecd448b7ca372c463cde3f4b8cf3e02c
AiYuki <khinhvu2707@gmail.com>
2021-06-12 15:13:41 +0700

    commit bài tập

---
+++ CaseStudy_Furoma_database/data/heThong.sql
@@ -0,0 +1,39 @@
+use furoma;
+insert into vi_tri(ten_vi_tri)
+values ("lễ tân"),
+("phục vụ"), ("chuyên viên"), 
+("giám sát"), 
+("quản lý"),
+("giám đốc");
+insert into trinh_do(trinh_do)
+values ("trung cấp"),
+("cao đẳng"),
+("đại học"),
+("sau đại học");
+insert into bo_phan(ten_bo_phan)
+values ("Sale – Marketing"),
+ ("Hành Chính"), 
+ ("Phục vụ"), 
+ ("Quản lý");
+ insert into loai_khach(ten_loai_khach)
+ values ("Diamond"), 
+ ("Platinium"), 
+ ("Gold"), 
+ ("Silver"),
+ ("Member");
+ insert into kieu_thue(ten_kieu_thue,gia)
+ value("năm",60000000),
+ ("tháng",5100000),
+ ("ngày",200000),
+ ("giờ",50000);
+ insert into loai_dich_vu(ten_loai_dich_vu)
+ values("villa"),
+ ("house"),
+ ("room");
+ insert into dich_vu_di_kem(ten_dich_vu_di_kem,gia,don_vi,trang_thai_kha_dung)
+ values("massage",200000,1,"on"), 
+ ("karaoke",150000,1,"on"), 
+ ("thức ăn",50000,1,"on"), 
+ ("nước uống",10000,1,"on"), 
+ ("thuê xe",200000,1,"on");
+ 
---
+++ CaseStudy_Furoma_database/model/table.sql
@@ -0,0 +1,97 @@
+create database furoma;
+use furoma;
+create table vi_tri(
+id_vi_tri int auto_increment primary key,
+ten_vi_tri varchar(45)
+);
+create table trinh_do(
+id_trinh_do int auto_increment primary key,
+trinh_do varchar(45)
+);
+create table bo_phan(
+id_bo_phan int auto_increment primary key,
+ten_bo_phan varchar(45)
+);
+create table nhan_vien(
+id_nhan_vien int auto_increment primary key,
+ho_ten_nhan_vien varchar(45),
+id_vi_tri int not null unique,
+id_trinh_do int not null unique,
+id_bo_phan int not null unique,
+ngay_sinh date,
+so_cmnd varchar(45),
+luong varchar(45),
+sdt varchar(45),
+email varchar(45),
+dia_chi varchar(45),
+foreign key (id_vi_tri) references vi_tri(id_vi_tri),
+foreign key (id_trinh_do) references trinh_do(id_trinh_do),
+foreign key (id_bo_phan) references bo_phan(id_bo_phan)
+);
+create table loai_khach(
+id_loai_khach int auto_increment primary key,
+ten_loai_khach varchar(45)
+);
+create table khach_hang(
+id_khach_hang int auto_increment primary key,
+id_loai_khach int not null,
+ho_ten_khach_hang varchar(45),
+ngay_sinh date,
+so_cmnd varchar(45),
+sdt varchar(45),
+email varchar(45),
+dia_chi varchar(45),
+foreign key (id_loai_khach) references loai_khach(id_loai_khach)
+);
+create table kieu_thue(
+id_kieu_thue int auto_increment primary key,
+ten_kieu_thue varchar(45),
+gia int not null
+);
+create table loai_dich_vu(
+id_loai_dich_vu int auto_increment primary key,
+ten_loai_dich_vu varchar(45)
+);
+create table dich_vu(
+id_dich_vu int primary key,
+ten_dich_vu varchar(45),
+dien_tich int,
+so_tang int,
+so_nguoi_toi_da varchar(45),
+chi_phi_thue varchar(45),
+id_kieu_thue int,
+id_loai_dich_vu int,
+trang_thai varchar(45),
+foreign key (id_kieu_thue) references kieu_thue(id_kieu_thue),
+foreign key (id_loai_dich_vu) references loai_dich_vu(id_loai_dich_vu)
+);
+create table hop_dong(
+id_hop_dong int primary key,
+id_nhan_vien int not null unique,
+id_khach_hang int not null unique,
+id_dich_vu int not null unique,
+ngay_lam_hop_dong date not null,
+ngay_ket_thuc date not null,
+tien_dat_coc int,
+tong_tien int not null,
+foreign key (id_nhan_vien) references nhan_vien(id_nhan_vien),
+foreign key (id_khach_hang) references khach_hang(id_khach_hang),
+foreign key (id_dich_vu) references dich_vu(id_dich_vu)
+);
+create table dich_vu_di_kem(
+id_dich_vu_di_kem int auto_increment primary key,
+ten_dich_vu_di_kem varchar(45) not null,
+gia int,
+don_vi int,
+trang_thai_kha_dung varchar(45)
+);
+create table hop_dong_chi_tiet(
+id_hop_dong_chi_tiet int primary key,
+id_hop_dong int not null,
+id_dich_vu_di_kem int not null,
+unique(id_hop_dong,id_dich_vu_di_kem),
+so_luong int,
+foreign key (id_hop_dong) references hop_dong(id_hop_dong),
+foreign key (id_dich_vu_di_kem) references dich_vu_di_kem(id_dich_vu_di_kem)
+);
+
